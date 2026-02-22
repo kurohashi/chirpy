61407b062862ad16166f9e8744de409134f746dd
Shubham Srivastava <shubham@supplynote.in>
2026-02-23 01:56:51 +0530

    3rd chapter done

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"strconv"
 	"sync/atomic"
 )
 
@@ -13,6 +13,20 @@ type apiConfig struct {
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func apiMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		println("API:", r.Method, r.URL.Path)
+
+		// future:
+		// auth
+		// rate limit
+		// headers
+
 		next.ServeHTTP(w, r)
 	})
 }
@@ -30,9 +44,27 @@ func main() {
 func routes(mux *http.ServeMux) {
 	hitConfig := apiConfig{}
 	mux.Handle("/app/", hitConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) { getMetrics(w, r, &hitConfig) })
-	mux.HandleFunc(("POST /reset"), func(w http.ResponseWriter, r *http.Request) { resetMetrics(w, r, &hitConfig) })
+
+	// mount api router
+	mux.Handle("/admin/", http.StripPrefix("/admin", newAdminRouter(&hitConfig)))
+	mux.Handle("/api/", http.StripPrefix("/api", newAPIRouter(&hitConfig)))
+}
+
+func newAPIRouter(hitConfig *apiConfig) http.Handler {
+	mux := http.NewServeMux()
+
 	mux.HandleFunc("GET /healthz", healthHandler)
+
+	return apiMiddleware(mux) // attach middleware once
+}
+
+func newAdminRouter(hitConfig *apiConfig) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) { getMetrics(w, r, hitConfig) })
+	mux.HandleFunc(("POST /reset"), func(w http.ResponseWriter, r *http.Request) { resetMetrics(w, r, hitConfig) })
+
+	return apiMiddleware(mux) // attach middleware once
 }
 
 func resetMetrics(res http.ResponseWriter, req *http.Request, config *apiConfig) {
@@ -42,9 +74,9 @@ func resetMetrics(res http.ResponseWriter, req *http.Request, config *apiConfig)
 }
 
 func getMetrics(res http.ResponseWriter, req *http.Request, config *apiConfig) {
-	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.Header().Set("Content-Type", "text/html")
 	res.WriteHeader(200)
-	response := "Hits: " + strconv.Itoa(int(config.fileserverHits.Load()))
+	response := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", config.fileserverHits.Load())
 	res.Write([]byte(response))
 }
 
